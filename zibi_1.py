0e988ca1aded7664b86d12eb6b977d2f06ae9b94
We1sh <90512906+We1sh@users.noreply.github.com>
2022-01-12 19:49:23 +0200

    Update zibi_1.py

--- zibi_1.py
+++ zibi_1.py
@@ -1,9 +1,11 @@
 def sum_3(a,b):
     for i in range(a,b):
-        if i%2==0 and i%2!=0:
-            return i
-    else:
-        print("Число неудачное")
+        if i%2==0:
+            print(i)
+            
+        else:
+            print("No-no")
+
 a=int(input())
 b=int(input())
-print(sum_3(a,b))
+sum_3(a,b)
